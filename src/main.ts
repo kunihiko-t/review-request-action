cc934cf63d9fb7ff7e9c151b8081f5272597be62
Kunihiko Tanaka <inverse.of.void.matrix@gmail.com>
2019-10-17 00:56:17 +0900

    modified import style

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,33 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from '@actions/core'
+import { context, GitHub } from '@actions/github'
 
 export async function run() {
   try {
     const welcomeMessage = core.getInput('welcome-message', { required: true }),
       repoToken = core.getInput('repo-token', { required: true }),
-      issue: { owner: string; repo: string; number: number } = github.context.issue
+      issue: { owner: string; repo: string; number: number } = context.issue
 
-    if (github.context.payload.action !== 'opened') {
-      console.log('No issue or pull request was opened, skipping');
-      return;
+    if (context.payload.action !== 'opened') {
+      console.log('No issue or pull request was opened, skipping')
+      return
     }
 
-    const client: github.GitHub = new github.GitHub(repoToken);
+    const client = new GitHub(repoToken)
     await client.issues.createComment({
       owner: issue.owner,
       repo: issue.repo,
       issue_number: issue.number,
       body: welcomeMessage
-    });
+    })
 
-  }
-  catch (error) {
-    core.setFailed(error.message);
-    throw error;
+    // await client.pulls.createReviewRequest(
+    //
+    // )
+
+  } catch (error) {
+    core.setFailed(error.message)
+    throw error
   }
 }
 
-run();
+run()
